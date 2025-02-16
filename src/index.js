e368be9b020951862cfea0b8029c78d5c5270d61
LozzaMcFozza <laurenwhike@hotmail.com>
2025-02-16 18:36:40 +0000

    search input finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello world");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-bar-input");
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFunctionElement = document.querySelector("#search-function");
+searchFunctionElement.addEventListener("submit", handleSearchSubmit);
